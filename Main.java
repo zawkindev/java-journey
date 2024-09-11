5d14331ef316ed670203f3fb7baf2a72b3ba4a20
zawkindev <zawkindev@gmail.com>
2024-09-11 12:50:30 +0500

    feat: data types

--- Main.java
+++ Main.java
@@ -1,5 +1,15 @@
 public class Main {
-    public static void main(String[] args){
-       System.out.println("Hello world"); 
+    public static void main(String[] args) {
+        String name = "Asuna";
+        int age = 18;
+        float height = 166.4f;
+        char gender = 'F';
+        boolean isWaifu = true; 
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Gender: " + gender);
+        System.out.println("isWaifu: " + isWaifu);
     }
 }
